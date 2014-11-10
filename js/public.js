7a23ef22c5fb8b8f94a22e9182a1300c47386f71
Profitshare <Profitshare@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-10 12:39:39 +0000

    v2.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-profitshare/trunk@1022979 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/public.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-	$("a.pslinks").mouseover(function() {
-		jQuery(this).children(".tip").show();
-	});
-        $("a.pslinks").mouseout(function() {
-		jQuery(this).children(".tip").hide();
-	});
-});
